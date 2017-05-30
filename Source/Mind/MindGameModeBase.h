fcf07d90dda26124f7bb0132016866a06c65c8f0
Kim Frost <kim@thatworks.dk>
2017-05-30 21:46:18 +0100

    ()

---
+++ Source/Mind/MindGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MindGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIND_API AMindGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
